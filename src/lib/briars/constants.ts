6b7f5d2427b41c8c0292dc41c87005f12322441f
j03l5k1 <51360672+j03l5k1@users.noreply.github.com>
2026-02-27 16:11:47 +1100

    Rename storage key constants for clarity

--- src/lib/briars/constants.ts
+++ src/lib/briars/constants.ts
@@ -3,6 +3,6 @@ export const TEAM_MATCH_TEXT = "briars";
 export const DEFAULT_TIMEZONE = "Australia/Sydney";
 export const CURRENT_SEASON = 2026;
 
-export const TEAM_PIN_STORAGE_KEY = "briars_team_pin";
-export const PLAYER_NAME_STORAGE_KEY = "briars_player_name";
-export const PIN_OK_STORAGE_KEY = "briars_pin_ok";
+export const LS_PIN_OK = "briars_pin_ok";
+export const LS_TEAM_PIN = "briars_team_pin";
+export const LS_PLAYER_NAME = "briars_player_name";

f307285e960a955b146f2ae6eb00fdfb385d1e89
j03l5k1 <51360672+j03l5k1@users.noreply.github.com>
2026-02-27 16:02:37 +1100

    Add constants for team and player configuration

---
+++ src/lib/briars/constants.ts
@@ -0,0 +1,8 @@
+export const TEAM_DISPLAY_NAME = "Briars";
+export const TEAM_MATCH_TEXT = "briars";
+export const DEFAULT_TIMEZONE = "Australia/Sydney";
+export const CURRENT_SEASON = 2026;
+
+export const TEAM_PIN_STORAGE_KEY = "briars_team_pin";
+export const PLAYER_NAME_STORAGE_KEY = "briars_player_name";
+export const PIN_OK_STORAGE_KEY = "briars_pin_ok";
